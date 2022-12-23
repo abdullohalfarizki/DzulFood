47075f6c686b4c17552bd4a2e809511ddf687b61
abdul <abdullohalfarizki11@gmail.com>
2022-12-24 03:39:23 +0700

    Membagi subfile file komponen web

--- index.php
+++ index.php
@@ -12,64 +12,13 @@
 <body style="height: 3000px;">
 
     <!-- Header -->
-    <nav class="navbar navbar-expand navbar-dark bg-info sticky-top">
-        <div class="container-lg">
-            <a class="navbar-brand" href="#"><i class="bi bi-circle-square"></i> DzulFood</a>
-            <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-                <ul class="navbar-nav">
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Username
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end mt-2">
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-person-fill"></i> Profile</a></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-gear"></i> Setting</a></li>
-                            <li><a class="dropdown-item" href="#"><i class="bi bi-box-arrow-in-left"></i> Logout</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
+    <?php include "header.php" ?>
     <!-- End Header -->
 
     <div class="container-lg">
         <div class="row">
             <!-- Sidebar -->
-            <div class="col-lg-3">
-                <nav class="navbar navbar-expand-lg bg-light rounded border mt-2">
-                    <div class="container-fluid">
-                        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel" style="width: 250px;">
-                            <div class="offcanvas-header">
-                                <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
-                                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                            </div>
-                            <div class="offcanvas-body">
-                                <ul class="navbar-nav nav-pills  flex-column justify-content-end flex-grow-1">
-                                    <li class="nav-item">
-                                        <a class="nav-link active link-light bg-info" aria-current="page" href="#"><i class="bi bi-houses"></i> Home</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link dark" href="#"><i class="bi bi-cart-check"></i> Order</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link dark" href="#"><i class="bi bi-person-fill-check"></i> Customer</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link dark" href="#"><i class="bi bi-card-checklist"></i> Product</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link link dark" href="#"><i class="bi bi-file-earmark-bar-graph"></i> Report</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
+            <?php include "sidebar.php" ?>
             <!-- End Sidebar -->
 
             <!-- Content -->
@@ -86,13 +35,11 @@
                 </div>
             </div>
             <!-- End Content -->
-
         </div>
 
         <!-- Footer -->
-        <div class="fixed-bottom text-center p-2 bg-info">
-            &copy; DzulFood - 2022
-        </div>
+        <?php include "footer.php" ?>
+        <!-- End Footer -->
 
     </div>
 
